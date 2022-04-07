0c5331725b77f27b35e868f0837127f1a8814948
jan van foreest <30220@ma-web.nl>
2022-04-07 10:16:45 +0200

    gpd-unity-1

---
+++ Assets/stuitercode.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stuitercode : MonoBehaviour
+{
+    public Rigidbody rb;
+    public bool Collision;
+    public int bounceheight = 20;
+    public Vector3 bounceforce = 15f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+        {
+            Transform.Position();
+        }
+            
+    }
+}
